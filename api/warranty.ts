aa75aa2ccdeb1b8c6446eab08be83de58899d376
苏向东 <suxiangdong@zcool.com.cn>
2021-03-01 18:44:47 +0800

    add

--- api/warranty.ts
+++ api/warranty.ts
@@ -1,5 +1,17 @@
 import {domain} from './proxy'
 export async function queryWarranties(phone: string): Promise<Response> {
-    const url = domain.concat('/api/warranty?phone='.concat(phone));
-    return fetch(url)
-  }
+  const url = domain.concat('/api/warranty?phone='.concat(phone));
+  return fetch(url)
+}
+
+export async function registerWarranties(params: {name: string, phone: string, sn: string}): Promise<Response> {
+  const url = domain.concat('/api/warranty');
+  return fetch(url, {
+    method: "POST",
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  })
+}
---
+++ pages/warranty/index.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import 'antd/dist/antd.css'
+import { Form, message, Input, Button } from 'antd';
+import styled from 'styled-components'
+import {registerWarranties} from '../../api/warranty'
+
+const Div = styled.div`
+  margin: 20px;
+`
+
+const Header = styled.div`
+  font-size: 18px;
+  text-align: center;
+  margin-top: 30px;
+`
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 24,
+  },
+};
+/* eslint-disable no-template-curly-in-string */
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const Register = (props: {query: {sn: string}}) => {
+
+  const onFinish = async (values) => {
+    const res = await (await registerWarranties(values)).json()
+    if (res.result) {
+      message.success('注册成功');
+    } else {
+      message.error('res.msg');
+    }
+  };
+
+  return (
+    <>
+    <Header>
+      智博士质保注册
+    </Header>
+    <Div>
+      <Form {...layout}
+        layout="vertical"
+        name="nest-messages"
+        onFinish={onFinish} 
+        initialValues={{sn: props.query.sn}}
+        validateMessages={validateMessages}>
+        <Form.Item
+          name="sn"
+          label="产品编号"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
+        <Form.Item
+          name="name"
+          label="姓名"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="手机号"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
+          <Button type="primary" htmlType="submit">
+            注册
+          </Button>
+        </Form.Item>
+      </Form>
+    </Div>
+    </>
+  );
+};
+
+export default Register
+
+export async function getServerSideProps(context) {
+  return {
+    props: {query: context.query},
+  }
+}
