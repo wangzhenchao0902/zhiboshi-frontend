b1d34c136498394277416e29cc239636a53d2795
wangzhenchao <693892038@qq.com>
2021-12-10 01:06:09 +0800

    updapte

--- shared/router.ts
+++ shared/router.ts
@@ -3,7 +3,7 @@ export const LINK_ROUTERS = [
   { name: '首页', path: '/', key: 'index', },
   { name: '案例中心', path: '/case', key: 'case' },
   { name: '新闻中心', path: '/news', key: 'news' },
-  { name: '质保查询', path: '/anti-fake', key: 'fake' },
+  { name: '车衣质保查询', path: '/anti-fake', key: 'fake' },
   { name: '车窗膜质保查询', path: '/chechuangmo', key: 'chechuangmo' },
   { name: '报价查询', path: '/offer', key: 'offer' },
   { name: '关于我们', path: '/about', key: 'about' }
